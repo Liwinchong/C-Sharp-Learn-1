237557725d2d6ae191cb4e1ae386646c6125f884
liwenchong <liwinchong@gmail.com>
2022-03-26 23:47:03 +0800

    First Learn (2022.3.26)

---
+++ LearnCSharp/SayHello/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayHello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestConsoleProperty();
+            Console.WriteLine("Hello!");
+            Console.ReadKey();
+        }
+
+        static void TestConsoleProperty()
+        {
+            Console.Title = "Current Time : " + DateTime.Now;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+        }
+    }
+}
