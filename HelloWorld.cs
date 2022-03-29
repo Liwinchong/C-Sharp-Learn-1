d929e2ddbd82435d51f2c5e58d00d48c329df767
liwenchong <liwinchong@gmail.com>
2022-03-29 23:38:48 +0800

    2

---
+++ HelloWorld.cs
@@ -0,0 +1,10 @@
+using System;
+public class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello, World!");
+		Console.WriteLine("Press any key to quit...");
+		Console.ReadKey();
+	}
+}
--- LearnCSharp/SayHello/Program.cs
+++ LearnCSharp/SayHello/Program.cs
@@ -12,7 +12,11 @@ namespace SayHello
         {
             TestConsoleProperty();
             Console.WriteLine("Hello!");
+            TestInputAndOutput();
+            Console.Beep();
             Console.ReadKey();
+            //Console.ReadKey(true);
+            //Console.ReadKey();
         }
 
         static void TestConsoleProperty()
@@ -21,5 +25,15 @@ namespace SayHello
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.BackgroundColor = ConsoleColor.DarkBlue;
         }
+
+        static void TestInputAndOutput()
+        {
+            Console.Write("Please input a string:");
+            String userInput = Console.ReadLine();
+
+            Console.WriteLine("User Input:\t{0}   \nLength:{1}", userInput, userInput.Length);
+        }
+
+
     }
 }
